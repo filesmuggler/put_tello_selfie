a9731b74af309212e3b19b57b75fd0d6056967b4
filesmuggler <krzste09@outlook.com>
2021-06-14 16:41:26 +0200

    refactor

--- main2.py
+++ main2.py
@@ -8,12 +8,6 @@ from threading import Thread
 matrix_coefficients = np.array([[929.562627, 0.000000, 487.474037], [0.000000, 928.604856, 363.165223], [0.000000, 0.000000, 1.000000]])
 distortion_coefficients = np.array([-0.016272, 0.093492, 0.000093, 0.002999, 0.000000])
 
-def connect_to_wifi(self, ssid, password):
-    """Connects to the Wi-Fi with SSID and password.
-    After this command the tello will reboot.
-    Only works with Tello EDUs.
-    """
-    return self.send_command_without_return('ap %s %s' % (ssid, password))
 
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
 arucoParams = cv2.aruco.DetectorParameters_create()
@@ -41,18 +35,11 @@ while diff<40:
             # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
             rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.05, matrix_coefficients,
                                                                        distortion_coefficients)
-            (rvec - tvec).any()  # get rid of that nasty numpy value array error
-
-            #cv2.aruco.drawDetectedMarkers(frame_read.frame, corners, ids)
-            cv2.aruco.drawDetectedMarkers(frame_read.frame, rejected, borderColor=(100, 0, 240))
-            cv2.aruco.drawAxis(frame_read.frame, matrix_coefficients, distortion_coefficients, rvec, tvec,0.01)  # Draw Axis
-
             print(ids[i],"position:", tvec)
+
     cv2.imshow('Live Video', frame_read.frame)
     video.write(frame_read.frame)
     cv2.waitKey(1)
-
-    #print(ids,rejected)
 
 
 video.release()
@@ -60,4 +47,3 @@ video.release()
 drone.streamoff()
 cv2.destroyAllWindows()
 drone.end()
-#time.sleep(5)
